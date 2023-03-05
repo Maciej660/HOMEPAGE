cbd3f7abc3c350f4a4f5cfff19f034f5aca365ab
Maciej.Konitz <maciej.konitz@wp.pl>
2023-03-05 20:54:43 +0100

    toggleBackground, welcome,init, const added

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
-console.log("Szanowni Państwo! Ten kod jest już w repozytorium git");
+{
+    const welcome = () => {
+    console.log("Szanowni Państwo! Ten kod jest już w repozytorium git");
+    }
 
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
-let themeName = document.querySelector(".js-themeName");
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const themeName = document.querySelector(".js-themeName");
+        body.classList.toggle("silverBackground");
+        themeName.innerText = body.classList.contains("silverBackground")
+            ? "na biały"
+            : "na szary";
+    };
 
+const init =() => {
+const button = document.querySelector(".js-button");
+button.addEventListener("click",toggleBackground);
 
-button.addEventListener("click", () => {
-    body.classList.toggle("silverBackground");
-    
-    themeName.innerText = body.classList.contains("silverBackground")
-        ? "na biały"
-        : "na szary";
-});
+welcome();
+};
 
+init();
 
-
-
-
-
-
-
-
-
-
-
+}
